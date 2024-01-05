1056c6651535bc0127cbbe157feabdbb3a91428e
daerbevoli <sameer.baruwal@student.uantwerpen.be>
2024-01-05 12:19:29 +0100

    Code cleanup and annotations

--- project/chess_utilities/chessEvalNeuralNet.py
+++ project/chess_utilities/chessEvalNeuralNet.py
@@ -1,47 +1,34 @@
 import torch
-from torch import optim
-from torch.utils.data import TensorDataset, DataLoader
-from training import train, test
 import numpy as np
-
+from torch.utils.data import TensorDataset, DataLoader
+import torch.optim as optim
 from project.chess_utilities.models.models import ChessEvaluationNetwork
+from project.chess_utilities.training import train, test
 
 if __name__ == "__main__":
-    # Initialize data
-
-    # Training
+    # Load training data
     X_train = torch.tensor(np.load("C:/Users/samee/OneDrive/Desktop/semester 5/Artificial Intelligence"
-                                   "/Lab4/chess_framework_student/project/chess_utilities/data/training/positions.npy")).to(
-        torch.float32)
+                                   "/Lab4/chess_framework_student/project/chess_utilities/data/training/positions.npy")).to(torch.float32)
     y_train = torch.tensor(np.load("C:/Users/samee/OneDrive/Desktop/semester 5/Artificial Intelligence"
-                                   "/Lab4/chess_framework_student/project/chess_utilities/data/training/results.npy")).to(
-        torch.float32)
+                                   "/Lab4/chess_framework_student/project/chess_utilities/data/training/results.npy")).to(torch.float32)
     print("Training data loaded")
 
-    # Testing
+    # Load testing data
     X_test = torch.tensor(np.load("C:/Users/samee/OneDrive/Desktop/semester 5/Artificial Intelligence"
-                                  "/Lab4/chess_framework_student/project/chess_utilities/data/testing/positions.npy")).to(
-        torch.float32)
+                                   "/Lab4/chess_framework_student/project/chess_utilities/data/testing/positions.npy")).to(torch.float32)
     y_test = torch.tensor(np.load("C:/Users/samee/OneDrive/Desktop/semester 5/Artificial Intelligence"
-                                  "/Lab4/chess_framework_student/project/chess_utilities/data/testing/results.npy")).to(
-        torch.float32)
+                                   "/Lab4/chess_framework_student/project/chess_utilities/data/testing/results.npy")).to(torch.float32)
     print("Testing data loaded")
 
-    # Create a TensorDataset from the two tensors
+    # Create training dataset and dataloader
     train_dataset = TensorDataset(X_train, y_train)
-
-    # Create a DataLoader from the dataset
     batch_size = 512
     shuffle = True
     train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=shuffle)
 
-    # Create a TensorDataset from the two tensors
+    # Create testing dataset and dataloader
     test_dataset = TensorDataset(X_test, y_test)
-
-    # Create a DataLoader from the dataset
-    batch_size = 512
-    shuffle = True
-    test_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=shuffle)
+    test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=shuffle)
 
     # Instantiate the model
     model = ChessEvaluationNetwork()
@@ -50,19 +37,14 @@ if __name__ == "__main__":
     optimizer = optim.Adam(model.parameters(), lr=0.001)
 
     print("Training starts")
-
     train(model, optimizer, train_dataloader)
-
-    print("Training complete")
-
-    print()
+    print("Training complete\n")
 
     print("Testing starts")
-
     test(model, test_dataloader)
-
-    print("testing complete")
+    print("Testing complete")
 
     # Save the model
-    torch.save(model.state_dict(), "C:/Users/samee/OneDrive/Desktop/semester 5/Artificial Intelligence"
-                                   "/Lab4/chess_framework_student/project/chess_utilities/models/model.pth")
+    model_path = "C:/Users/samee/OneDrive/Desktop/semester 5/Artificial " \
+                 "Intelligence/Lab4/chess_framework_student/project/chess_utilities/models/model.pth"
+    torch.save(model.state_dict(), model_path)
